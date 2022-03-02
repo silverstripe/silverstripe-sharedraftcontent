612db19a7175efe96998ce3eb6abd06231fd24c4
Steve Boyd <emteknetnz@gmail.com>
2022-03-02 17:14:54 +1300

    NEW Session grant files without permissions (#150)
    * NEW Session grant files without permissions
    
    * Update .travis.yml
    
    Co-authored-by: Michal Kleiner <mk@011.nz>
    
    Co-authored-by: Michal Kleiner <mk@011.nz>

---
+++ src/Extensions/ShareDraftContentFileShortcodeProviderExtension.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace SilverStripe\ShareDraftContent\Extensions;
+
+use SilverStripe\Assets\Shortcodes\FileShortcodeProvider;
+use SilverStripe\Assets\File;
+use SilverStripe\Control\Controller;
+use SilverStripe\Core\Extension;
+use SilverStripe\ShareDraftContent\Controllers\ShareDraftController;
+
+class ShareDraftContentFileShortcodeProviderExtension extends Extension
+{
+    /**
+     * @param bool $grant
+     * @param File|null $record
+     * @param array|null $args Shortcode args as passed to FileShortcodeProvider::handle_shortcode()
+     */
+    public function updateGrant(bool &$grant, ?File $record = null, ?array $args = null): void
+    {
+        if ($grant) {
+            return;
+        }
+        $session = $session = Controller::curr()->getRequest()->getSession();
+        if (!$session) {
+            return;
+        }
+        if (!ShareDraftController::getIsViewingPreview()) {
+            return;
+        }
+        if (!$record && $args) {
+            $record = FileShortcodeProvider::find_shortcode_record($args);
+        }
+        if ($record) {
+            $grant = !$record->hasRestrictedAccess();
+        }
+    }
+}
