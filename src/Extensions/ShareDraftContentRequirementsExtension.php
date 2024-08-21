18c96c5ef67739d5df305d4877eb88de282fddd9
Guy Sartorelli <guy.sartorelli@silverstripe.com>
2024-08-21 12:16:42 +1200

    API Deprecate extension to be replaced with config

--- src/Extensions/ShareDraftContentRequirementsExtension.php
+++ src/Extensions/ShareDraftContentRequirementsExtension.php
@@ -3,14 +3,24 @@
 namespace SilverStripe\ShareDraftContent\Extensions;
 
 use SilverStripe\CMS\Controllers\CMSMain;
+use SilverStripe\Dev\Deprecation;
 use SilverStripe\ORM\DataExtension;
 use SilverStripe\View\Requirements;
 
 /**
  * @extends DataExtension<CMSMain>
+ * @deprecated 5.3.0 Will be replaced with YAML configuration
  */
 class ShareDraftContentRequirementsExtension extends DataExtension
 {
+    public function __construct()
+    {
+        Deprecation::withNoReplacement(
+            fn () => Deprecation::notice('5.3.0', 'Will be replaced with YAML configuration', Deprecation::SCOPE_CLASS)
+        );
+        parent::__construct();
+    }
+
     public function init()
     {
         Requirements::css('silverstripe/sharedraftcontent: client/dist/styles/bundle-cms.css');
