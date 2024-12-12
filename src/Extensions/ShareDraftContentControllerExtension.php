6cccd255a7a39130655e30102d497f4dcd946b10
Guy Sartorelli <36352093+GuySartorelli@users.noreply.github.com>
2024-12-12 17:29:54 +1300

    FIX Don't rely on session for current record ID (#272)

--- src/Extensions/ShareDraftContentControllerExtension.php
+++ src/Extensions/ShareDraftContentControllerExtension.php
@@ -53,9 +53,27 @@ class ShareDraftContentControllerExtension extends Extension
      */
     public function getShareDraftLinkAction()
     {
-        if ($this->owner->config()->get('url_segment')) {
-            return $this->owner->Link('MakeShareDraftLink');
+        $owner = $this->getOwner();
+        if (!$owner->config()->get('url_segment')) {
+            return '';
         }
-        return '';
+        $id = $this->getRecordID();
+        if (!$id) {
+            return '';
+        }
+        return $owner->Link(Controller::join_links('MakeShareDraftLink', $id));
+    }
+
+    private function getRecordID(): ?int
+    {
+        $owner = $this->getOwner();
+        if ($owner->hasMethod('currentRecordID')) {
+            return $owner->currentRecordID();
+        }
+        // Could be a non-LeftAndMain controller, since the extension is applied directly to Controller
+        if ($owner->hasMethod('CurrentPage')) {
+            return $owner->CurrentPage()?->ID;
+        }
+        return null;
     }
 }
