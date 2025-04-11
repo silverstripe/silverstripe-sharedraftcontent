6a2a6b13dd6d961c24c221e91e3b18788e1e7fdb
Hitaishi <hitaishi.marfatiya@gmail.com>
2025-04-11 04:36:24 +0000

    Fix

--- src/Controllers/ShareDraftController.php
+++ src/Controllers/ShareDraftController.php
@@ -91,10 +91,6 @@ class ShareDraftController extends Controller
         $page = Versioned::get_by_stage(SiteTree::class, Versioned::DRAFT)
             ->byID($shareToken->PageID);
 
-        if (!$page) {
-            return $this->errorPage();
-        }
-
         $this->extend('updatePage', $page);
 
         if (!$page) {
